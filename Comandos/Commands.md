db819f9148768ad93c4737c8c67483e15ebdd09d
Alexander <paulo.alexander12@gmail.com>
2020-07-04 18:36:15 -0500

    Section 1 - Creating project

---
+++ Comandos/Commands.md
@@ -0,0 +1,13 @@
+# RUN Directly
+C:\DEV> java Test.java
+
+
+#Compile and run 
+C:\DEV> javac Test.java
+java -classpath C:\DEV Test
+
+#Compile 
+javac -verbose Test.java
+
+#Delete 
+del Test.class
---
+++ ProjectJavaSE11/src/com/company/section1/TheStartComplete.java
@@ -0,0 +1,18 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 2: Create an executable Java program
+*/
+package com.company.section1;
+
+class TheStartComplete {
+    public static void main(String[] args) {
+        System.out.println("This executes 1");
+    }
+}
+
+// Demonstrate multiple classes in one source file
+class Next {
+    public static void main(String args) {
+        System.out.println("This executes 2");
+    }
+}
