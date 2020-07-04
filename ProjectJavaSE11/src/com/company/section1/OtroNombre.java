908a628386b16526737f5d3bbe27e88704dfab6b
Alexander <paulo.alexander12@gmail.com>
2020-07-04 18:32:46 -0500

    Setion 1 - Creating project

---
+++ ProjectJavaSE11/src/com/company/section1/OtroNombre.java
@@ -0,0 +1,12 @@
+package com.company.section1;
+
+/**
+ * Developer : Alexander Chiran (paulo.alexander12@gmail.com)
+ * Cuando una clase es pública debe llevar el mismo nombre del archivo, en este caso al quitar la palabra reservada public el problema se soluciona
+ */
+class OtraPrueba {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Prueba de Intellij 2");
+    }
+}
---
+++ ProjectJavaSE11/src/com/company/section1/Test.java
@@ -0,0 +1,8 @@
+package com.company.section1;
+
+public class Test {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Prueba de Intellij");
+    }
+}
