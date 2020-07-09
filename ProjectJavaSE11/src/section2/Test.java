da677f38a967b294e57a66ffac623811a5e07d5e
Alexander <paulo.alexander12@gmail.com>
2020-07-08 19:11:59 -0500

    Primitives types

---
+++ ProjectJavaSE11/src/section2/Test.java
@@ -0,0 +1,15 @@
+package section2;
+
+public class Test {
+    /**
+     * Primitive types
+     */
+    private int ivalor=0;
+    private char cvalor='A';
+    private byte bvalor=127;
+    private short svalor=32767;
+    private long lvalor=999999;
+    private float fvalor=1.0f;
+    private double dvalor=2.2;
+    private boolean bovalor=true;
+}
