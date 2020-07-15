580ebecd50d4c5832c51e23a57dab1b14d3efc57
Alexander Chiran <paulo.alexander12@gmail.com>
2020-07-15 18:14:41 -0500

    Java 11 - Section 2 - Local Varibale Type Inference

--- ProjectJavaSE11/src/section2/acme/Test.java
+++ ProjectJavaSE11/src/section2/acme/Test.java
@@ -1,5 +1,14 @@
 package section2.acme;
 
+/**
+ * Local Variable Type Inference
+ *  https://openjdk.java.net/projects/amber/LVTIFAQ.html#:~:text=var%20can%20be%20used%20in,static%20type%20of%20the%20variable.
+ *
+ *  https://docs.oracle.com/en/java/javase/13/language/local-variable-type-inference.html
+ *
+ *  https://www.baeldung.com/java-var-lambda-params
+ *
+ */
 /*
 Correct Answer: Line 3 fails to compile when a double is added to an int and the result is a double which does not fit
  into the result variable which was inferred to be an int.
--- ProjectJavaSE11/src/section2/acme/Var.java
+++ ProjectJavaSE11/src/section2/acme/Var.java
@@ -1,4 +1,12 @@
 package section2.acme;
-
+/**
+ * Local Variable Type Inference
+ *  https://openjdk.java.net/projects/amber/LVTIFAQ.html#:~:text=var%20can%20be%20used%20in,static%20type%20of%20the%20variable.
+ *
+ *  https://docs.oracle.com/en/java/javase/13/language/local-variable-type-inference.html
+ *
+ *  https://www.baeldung.com/java-var-lambda-params
+ *
+ */
 public class Var {
 }
