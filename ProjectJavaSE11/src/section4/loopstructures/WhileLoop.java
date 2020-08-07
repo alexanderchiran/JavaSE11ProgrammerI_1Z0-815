0eb2798daa0b5f5e8a639b680372662395c41988
Alexander Chiran <paulo.alexander12@gmail.com>
2020-08-07 14:21:17 -0500

    Java SE 11 Programmer I_1Z0-815- course - Paulo Alexander Chiran

---
+++ ProjectJavaSE11/src/section4/loopstructures/WhileLoop.java
@@ -0,0 +1,42 @@
+package section4.loopstructures;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        // set up a boolean that while be the condition for our while loop
+        boolean keepGoing = true;
+        int iterations = 0;
+
+        while (keepGoing) {
+
+            if (++iterations > 2) break;
+            if (iterations == 2) continue;
+            System.out.println("Iteration: " + iterations +", keepGoing = " + keepGoing);
+            do {
+                System.out.println("--- Doing something in the nested loop," + " iterations =" + iterations);
+                System.out.println("--- END OF DO WHILE LOOP");
+            } while (iterations < 0);
+
+        }  // end while
+    }
+}
+
+class WhileLoop2 {
+    public static void main(String[] args) {
+
+        // set up a boolean that while be the condition for our while loop
+        boolean keepGoing = true;
+        int iterations = 0;
+
+        while (keepGoing) {
+
+            if (++iterations > 2) keepGoing=false;
+            System.out.println("Iteration: " + iterations +", keepGoing = " + keepGoing);
+            do {
+                System.out.println("--- Doing something in the nested loop," + " iterations =" + iterations);
+                System.out.println("--- END OF DO WHILE LOOP");
+            } while (iterations < 0);
+
+        }  // end while
+    }
+}
